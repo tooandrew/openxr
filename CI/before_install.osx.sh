085c831646cbc8a9fcbe41119b8d758ba59016bf
corristo <corrmage@gmail.com>
2020-11-09 21:33:59 +0000

    Update macOS dependencies link

--- CI/before_install.osx.sh
+++ CI/before_install.osx.sh
@@ -5,5 +5,5 @@ command -v ccache >/dev/null 2>&1 || brew install ccache
 command -v cmake >/dev/null 2>&1 || brew install cmake
 command -v qmake >/dev/null 2>&1 || brew install qt
 
-curl -fSL -R -J https://www.dropbox.com/s/86ei812d3knuuki/openmw-deps-f8918dd.zip?dl=1 -o ~/openmw-deps.zip
+curl -fSL -R -J http://downloads.openmw.org/osx/dependencies/openmw-deps-f8918dd.zip -o ~/openmw-deps.zip
 unzip -o ~/openmw-deps.zip -d /private/tmp/openmw-deps > /dev/null
